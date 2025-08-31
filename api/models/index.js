1f9adf9c62baf2ef193681ac2fe8e2d03edd3b61
21424359 <21424359@students.latrobe.edu.au>
2025-08-31 20:36:36 +1000

    Add migration to alter contact and phone tables

--- api/models/index.js
+++ api/models/index.js
@@ -23,4 +23,36 @@ db.sequelize = sequelize;
 db.contacts = require("./contact.model.js")(sequelize, Sequelize);
 db.phones = require("./phone.model.js")(sequelize, Sequelize);
 
+
+// Add migrations to alter the already-live tables
+async function runMigrations() {
+  const queryInterface = sequelize.getQueryInterface();
+  try {
+    console.log("Adding address column");
+    await queryInterface.addColumn("contacts", "address", {
+      type: Sequelize.STRING,
+      allowNull: true
+    }).catch(() => console.log("Column 'address' already exists in contacts table"));
+
+    console.log("Updating phone columns");
+    await queryInterface.renameColumn("phones", "name", "phone_type")
+      .catch(() => console.log("Column 'name' already renamed"));
+    await queryInterface.renameColumn("phones", "number", "phone_number")
+      .catch(() => console.log("Column 'number' already renamed"));
+
+    console.log("Migration complete");
+  } catch (err) {
+    console.error("Migration failed!!", err);
+  }
+}
+
+// Trigger migration after Sequelize connects 
+sequelize.authenticate() .then(() => { 
+  console.log("Database connected"); 
+  runMigrations(); 
+}) 
+.catch(err => { 
+  console.err("Unable to connect to database. ", err); 
+});
+
 module.exports = db;
