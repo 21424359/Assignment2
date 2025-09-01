399cc83f80e16bec11b71203313582af3e41eea0
21424359 <21424359@students.latrobe.edu.au>
2025-09-02 08:43:25 +1000

    Update NewContact.js - update to add company

--- frontend/src/components/NewContact.js
+++ frontend/src/components/NewContact.js
@@ -5,6 +5,9 @@ function NewContact(props) {
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
 
+    const [company_name, set_company_name] = useState('');
+    const [company_address, set_company_address] = useState('');
+
     async function createContact(e) {
         e.preventDefault();
 
@@ -23,10 +26,23 @@ function NewContact(props) {
 
         if (data.id) {
             setContacts([...contacts, data]);
+            company = await fetch('http://localhost/api/contacts/'+ data.id + '/company', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    company_name,
+                    company_address,
+
+                })
+            })
         }
 
         setName('');
         setAddress('');
+        set_company_name('');
+        set_company_address('');
     }
 
 	return (
@@ -35,6 +51,10 @@ function NewContact(props) {
             <input type='text' placeholder='Enter Contact Name' onChange={(e) => setName(e.target.value)} value={name}/>
             <p>Contact Address:</p>
             <input type='text' placeholder='Enter Contact Address' onChange={(e) => setAddress(e.target.value)} value={address}/>
+            <p>Company Name:</p>
+            <input type='text' placeholder='Enter Contact Name' onChange={(e) => set_company_name(e.target.value)} value={company_name}/>
+            <p>Company Address:</p>
+            <input type='text' placeholder='Enter Contact Address' onChange={(e) => set_company_address(e.target.value)} value={company_address}/>
             <button className='button green' type='submit'>Create Contact</button>
         </form>
 	);
