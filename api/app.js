525ef340a2b773d1970fd3dfd82c0f7f40194d27
21424359 <21424359@students.latrobe.edu.au>
2025-09-02 21:38:44 +1000

    Add companies routes to expose the APIs

--- api/app.js
+++ api/app.js
@@ -29,6 +29,9 @@ require("./routes/contacts.routes")(app);
 require("./routes/phones.routes")(app);
 require("./routes/stats.routes")(app);
 
+// Add the company routes
+require("./routes/companies.routes")(app);
+
 // set port, listen for requests
 const PORT = 5000;
 app.listen(PORT, () => {
