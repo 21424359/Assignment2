f501de6288e0540672c75ec180f191aae6b56a7e
21424359 <21424359@students.latrobe.edu.au>
2025-09-02 22:34:15 +1000

    Update routes for delete and update

--- api/routes/companies.routes.js
+++ api/routes/companies.routes.js
@@ -1,3 +1,4 @@
+// This is created under the thought that there is a 1:1 relationship between company and contact
 module.exports = app => {
     const company = require("../controllers/companies.controller.js");
   
@@ -7,9 +8,9 @@ module.exports = app => {
   
     router.get("/contacts/:contactId/company", company.findOne);
   
-    router.put("/contacts/:contactId/company/:company_id", company.update);
+    router.put("/contacts/:contactId/company/", company.update);
   
-    router.delete("/contacts/:contactId/company/:company_id", company.delete);
+    router.delete("/contacts/:contactId/company/", company.delete);
   
     app.use('/api', router);
 };
