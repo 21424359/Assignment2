cbf561f4f49dcf8b1b2af3c07bd1de2dddcd1af3
21424359 <21424359@students.latrobe.edu.au>
2025-09-03 21:32:41 +1000

    Update contact.controller.js

--- api/controllers/contact.controller.js
+++ api/controllers/contact.controller.js
@@ -8,6 +8,7 @@ const Op = db.Sequelize.Op;
 exports.create = (req, res) => {
     const contact = {
         name: req.body.name,
+        address: req.body.address
     };
 
     Contacts.create(contact)
